5e595d250f0d7a408a7ccd40bfa25d24c000034d
Chris Z <ckz8780@gmail.com>
2020-02-11 13:20:40 +0000

    added sizes to product model and size selctor box to product template

--- products/models.py
+++ products/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
